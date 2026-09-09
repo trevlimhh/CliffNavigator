3d97b3d753abe85a839670f336e224fea4e68636
trevlimhh <41354887+trevlimhh@users.noreply.github.com>
2026-09-09 18:27:34 +0800

    Set runtime to edge in middleware configuration

--- middleware.ts
+++ middleware.ts
@@ -54,5 +54,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
+  runtime: "edge",
   matcher: ["/((?!_next/static|_next/image|favicon.ico|api/intake|api/simulate).*)"],
 };
